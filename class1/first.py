788c66fc883ab46e7a53d1b60eb2dd6ce2e794ce
Amir Rashid <66682973+aamirashid@users.noreply.github.com>
2024-10-20 22:43:33 +0500

    Add files via upload

---
+++ class1/first.py
@@ -0,0 +1,19 @@
+mess = "testing messages"
+Mess = '2nd testing'
+num:int=20
+num1 = 10
+num2:float
+
+num2 = 133.1496
+
+print(mess)
+print(Mess)
+print(num)
+print(num1)
+print(round(num2,2))
+
+print(f'{num2:0.2f}')
+
+number = 3.14159
+formatted_number = f"{number:.2f}"
+print(formatted_number)
